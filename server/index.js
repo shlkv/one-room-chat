86e244b79690be7c3ec64da813c72a7e45edb761
shlkv <shulyakov_sasha@mail.ru>
2023-11-24 14:16:41 +0300

    create initial interface

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server)
 
 const path = require('path');
 
@@ -10,6 +12,11 @@ app.get('/', (req, res) => {
   res.sendFile(filePath);
 });
 
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
